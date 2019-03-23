34f7c99202d6b2c742cabe10780e857b285a81cb
Laura Ferrando <lauraferrandof@gmail.com>
2019-03-23 11:15:33 +0100

    Create docs folder

---
+++ docs/precache-manifest.578093b9b43548a33ca9b3063d7e8850.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "ab4b36e00a946098da8e",
+    "url": "./static/js/main.caf2eff5.chunk.js"
+  },
+  {
+    "revision": "d031566972a8068d94a5",
+    "url": "./static/js/2.a6945a40.chunk.js"
+  },
+  {
+    "revision": "ab4b36e00a946098da8e",
+    "url": "./static/css/main.b2cea9f4.chunk.css"
+  },
+  {
+    "revision": "49041a6712df69a8bc2dd90bcccbafe1",
+    "url": "./index.html"
+  }
+];
